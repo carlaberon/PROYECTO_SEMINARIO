a384f1ff3ed5b11bb893069d46bca9e79703f56d
gabrielaven <avenda7oelias@gmail.com>
2024-10-18 02:03:25 -0300

    Eror

--- FRONTEND/src/ar/edu/unrn/seminario/gui/Inicio.java
+++ FRONTEND/src/ar/edu/unrn/seminario/gui/Inicio.java
@@ -8,8 +8,6 @@ import ar.edu.unrn.seminario.dto.ProyectoDTO;
 import ar.edu.unrn.seminario.dto.UsuarioDTO;
 import ar.edu.unrn.seminario.exception.DataEmptyException;
 import ar.edu.unrn.seminario.exception.NotNullException;
-import ar.edu.unrn.seminario.modelo.Rol;
-import ar.edu.unrn.seminario.modelo.Usuario;
 import ar.edu.unrn.seminario.dto.RolDTO;
 
 import java.awt.*;
