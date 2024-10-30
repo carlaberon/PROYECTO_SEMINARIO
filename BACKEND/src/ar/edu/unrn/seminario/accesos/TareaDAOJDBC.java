1d751b5bcc781087e9c172ec4164e9d56957fd73
gabrielaven <avenda7oelias@gmail.com>
2024-10-30 12:35:08 -0300

    tareadao

--- BACKEND/src/ar/edu/unrn/seminario/accesos/TareaDAOJDBC.java
+++ BACKEND/src/ar/edu/unrn/seminario/accesos/TareaDAOJDBC.java
@@ -23,16 +23,17 @@ public class TareaDAOJDBC implements TareaDao{
 			conn = ConnectionManager.getConnection();
 			
 			statement = conn
-					.prepareStatement("INSERT INTO tareas(nombre, proyecto, prioridad, usuario, estado, descripcion, fecha_inicio, fecha_fin)" + "VALUES(?, ?, ?, ?, ?, ?, ?, ?)");
+					.prepareStatement("INSERT INTO tareas(nombre, proyecto,usuario_propietario, prioridad, usuario, estado, descripcion, fecha_inicio, fecha_fin)" + "VALUES(?, ?, ?,?, ?, ?, ?, ?, ?)");
 		
 			statement.setString(1, tarea.getNombre());
 			statement.setString(2, tarea.getProyecto());
-			statement.setString(3, tarea.getPrioridad());
-			statement.setString(4, tarea.getUsuario());
-			statement.setBoolean(5, tarea.isEstado());
-			statement.setString(6, tarea.getDescripcion());
-			statement.setDate(7, java.sql.Date.valueOf(tarea.getInicio().toLocalDate()));
-			statement.setDate(8, java.sql.Date.valueOf(tarea.getFin().toLocalDate()));
+			statement.setString(3, tarea.getUsuarioPropietario());
+			statement.setString(4, tarea.getPrioridad());
+			statement.setString(5, tarea.getUsuario());
+			statement.setBoolean(6, tarea.isEstado());
+			statement.setString(7, tarea.getDescripcion());
+			statement.setDate(8, java.sql.Date.valueOf(tarea.getInicio().toLocalDate()));
+			statement.setDate(9, java.sql.Date.valueOf(tarea.getFin().toLocalDate()));
 			
 			int cant = statement.executeUpdate();
 		
