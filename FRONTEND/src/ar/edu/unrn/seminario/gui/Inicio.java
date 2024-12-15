178165290cb4d846eae580b73dc700bdff9aa63f
EzeRamos17 <hernan_ram99@hotmail.com>
2024-12-15 11:10:04 -0300

    a

--- FRONTEND/src/ar/edu/unrn/seminario/gui/Inicio.java
+++ FRONTEND/src/ar/edu/unrn/seminario/gui/Inicio.java
@@ -384,7 +384,7 @@ public class Inicio extends JFrame {
 		UsuarioDTO usuario;
 		ResourceBundle labels = ResourceBundle.getBundle("labels", new Locale("en")); 
 		try {
-			usuario = api.obtenerUsuario("sbrito");
+			usuario = api.obtenerUsuario("ldifabio");
 			api.setUsuarioActual(usuario.getUsername());
 			Inicio inicio = new Inicio(api);
 			inicio.setVisible(true);
